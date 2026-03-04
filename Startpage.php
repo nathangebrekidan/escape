b29986de00394b5b0db9660cbcbc58e4a063e033
Yannick-Muller <9029066@student.zadkine.nl>
2026-03-04 14:35:56 +0100

    Aanmeld test

--- Startpage.php
+++ Startpage.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Escape Room</title>
+  <title>Simlock</title>
   <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
 
-  <h1>Welkom</h1>
+  <h1>Welkom to Simlock reality!</h1>
   <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
     uitleg van jullie escape room.</p>
   <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
