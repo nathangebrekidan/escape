293497c820d12a4e95ae04fe93552519e6211cd9
Yannick-Muller <9029066@student.zadkine.nl>
2026-04-10 10:17:34 +0200

    Room in progress

--- rooms/room_1.php
+++ rooms/room_1.php
@@ -15,25 +15,46 @@ try {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Escape Room 3</title>
+  <title>Het schoollab</title>
   <link rel="stylesheet" href="../css/style.css">
   <script defer src="../js/app.js"></script>
 </head>
 
 <body class="backbody">
 
+<<<<<<< Updated upstream
 <header class="page-header">
   <h1>Escape Room 1</h1>
   <p>Oplossen voordat de klok aftelt - Succes!</p>
 </header>
 
 <div id="timer">05:00</div>
+=======
+<div id="timer">Loading...</div>
+>>>>>>> Stashed changes
 
 <div id="room">
-  <img src="../img/Tafel.png" alt="Table">
-  <img src="../img/Safe.png" alt="Kluis">
-  <img src="../img/Safe_open.png" alt="Kluis geopend">
+  <img id="door" src="../img/Door.png" alt="Deur" onclick="tryDoor()">
+  <img id="table" src="../img/Tafel.png" alt="Table">
+  <img id="table_two" src="../img/Tafel_2.png" alt="Tweede tafel">
+  <img id="safe_closed" src="../img/Safe.png" alt="Kluis" onclick="trySafe()">
+  <img id="safe_open" src="../img/Safe_open.png" alt="Kluis geopend" onclick="trySafe()">
+  <img id="key" src="../img/Key.png" alt="Sleutel" onclick="pickUpKey()">
+  <img id="key_card" src="../img/Key_Card.png" alt="Key Card" onclick="pickUpKeyCard()">
+  <img id="keypad_locked" src="../img/Keypad_locked.png" alt="Keypad gesloten" onclick="tryKeypad()">
+  <img id="keypad_unlocked" src="../img/Keypad_unlocked.png" alt="Keypad geopend" onclick="tryKeypad()">
+  <img id="locked_box" src="../img/Locked_box.png" alt="Gesloten doos" onclick="tryBox()">
+  <img id="opened_box" src="../img/Opened_box.png" alt="Geopende doos" onclick="tryBox()">
+  <img id="lockpick" src="../img/Lockpick.png" alt="Lockpick" onclick="pickUpLockpick()">
+  <img id="drawer_closed" src="../img/Drawer_closed.png" alt="Gesloten lade" onclick="tryDrawer()">
+  <img id="drawer_open" src="../img/Drawer_open.png" alt="Geopende lade" onclick="tryDrawer()">
+  <img id="lights" src="../img/Lights.png" alt="Licht" onclick="tryLight()">
+  <img id="lights_two" src="../img/Lights_two.png" alt="Tweede licht" onclick="tryLight()">
+  <img id="lightshadow" src="../img/Light_shadow.png" alt="Schaduw van het licht">
+  <img id="lightshadow_two" src="../img/Light_shadow_two.png" alt="Schaduw van het tweede licht">
 </div>
+
+<p id="keypadmessage"></p>
 
   <div class="container">
     <?php foreach ($riddles as $index => $riddle) : ?>
